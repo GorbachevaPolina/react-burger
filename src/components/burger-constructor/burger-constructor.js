7492450e38fc8c52d350396cd2d1a68d2e8ea404
GorbachevaPolina <gorbacheva.polina7394@gmail.com>
2022-11-26 16:08:00 +0300

    added propTypes

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import styles from './burger-constructor.module.css'
 import {ConstructorElement, DragIcon, Button, CurrencyIcon} from '@ya.praktikum/react-developer-burger-ui-components'
 
@@ -93,6 +94,25 @@ const BurgerConstructor = ({data}) => {
             </div>
         </section>
     )
+}
+
+BurgerConstructor.propTypes = {
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+                _id: PropTypes.string.isRequired,
+                name: PropTypes.string.isRequired,
+                type: PropTypes.string.isRequired,
+                proteins: PropTypes.number.isRequired,
+                fat: PropTypes.number.isRequired,
+                carbohydrates: PropTypes.number.isRequired,
+                calories: PropTypes.number.isRequired,
+                price: PropTypes.number.isRequired,
+                image: PropTypes.string.isRequired,
+                image_mobile: PropTypes.string,
+                image_large: PropTypes.string,
+                __v: PropTypes.number.isRequired
+        })
+    ).isRequired
 }
 
 export default BurgerConstructor;
--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import PropTypes from 'prop-types'
 import {Tab, CurrencyIcon, Counter} from "@ya.praktikum/react-developer-burger-ui-components"
 import styles from './burger-ingredients.module.css'
 
@@ -94,6 +95,25 @@ const BurgerIngredients = ({data}) => {
             </ul>
         </section>
     )
+}
+
+BurgerIngredients.propTypes = {
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+                _id: PropTypes.string.isRequired,
+                name: PropTypes.string.isRequired,
+                type: PropTypes.string.isRequired,
+                proteins: PropTypes.number.isRequired,
+                fat: PropTypes.number.isRequired,
+                carbohydrates: PropTypes.number.isRequired,
+                calories: PropTypes.number.isRequired,
+                price: PropTypes.number.isRequired,
+                image: PropTypes.string.isRequired,
+                image_mobile: PropTypes.string,
+                image_large: PropTypes.string,
+                __v: PropTypes.number.isRequired
+        })
+    ).isRequired
 }
 
 export default BurgerIngredients;
